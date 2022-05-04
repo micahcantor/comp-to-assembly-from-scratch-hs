f16218b9e07935f16ba873ba39fc5b112ac12d27
Micah Cantor <micahcantor01@gmail.com>
2022-05-04 12:18:00 -0400

    change facorial

--- data/js/factorial.js
+++ data/js/factorial.js
@@ -1,12 +1,12 @@
-function factorial(n) {
-    var result = 1;
-    while (n != 1) {
-        result = result * n;
-        n = n - 1;
-    }
-    return result;
-}
-
 function main() {
+    function factorial(n) {
+        var result = 1;
+        while (n != 1) {
+            result = result * n;
+            n = n - 1;
+        }
+        return result;
+    }
+
     assert(factorial(5) == 120);
 }
