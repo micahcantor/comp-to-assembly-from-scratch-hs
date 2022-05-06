e968b92614ddda69f43d9c51c6c584f03d367055
Micah Cantor <micahcantor01@gmail.com>
2022-05-05 23:38:03 -0500

    update math.s

--- data/arm/math.s
+++ data/arm/math.s
@@ -100,21 +100,6 @@ main:
   moveq r0, #'.'
   movne r0, #'F'
   bl putchar
-  ldr r0, =25
-  push {r0, ip}
-  ldr r0, =5
-  pop {r1, ip}
-  udiv r0, r0, r1
-  push {r0, ip}
-  ldr r0, =5
-  pop {r1, ip}
-  cmp r0, r1
-  moveq r0, #1
-  moveq r0, #0
-  cmp r0, #1
-  moveq r0, #'.'
-  movne r0, #'F'
-  bl putchar
   mov sp, fp
   mov r0, #0
   pop {fp, pc}
