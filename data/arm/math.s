27aa1377b23ef16999400ee74627fd89de82c4b0
Micah Cantor <micahcantor01@gmail.com>
2022-05-06 00:04:27 -0500

    update

--- data/arm/math.s
+++ data/arm/math.s
@@ -3,34 +3,24 @@ main:
   push {fp, lr}
   mov fp, sp
   push {r0, r1, r2, r3}
-  
-  // add 1+1
+  ldr r0, =1
+  push {r0, ip}
+  ldr r0, =1
+  pop {r1, ip}
+  cmp r0, r1
+  moveq r0, #1
+  moveq r0, #0
+  cmp r0, #1
+  moveq r0, #'.'
+  movne r0, #'F'
+  bl putchar
   ldr r0, =1
   push {r0, ip}
   ldr r0, =1
   pop {r1, ip}
   add r0, r0, r1
   push {r0, ip}
-
-  // == 2
   ldr r0, =2
-  pop {r1, ip}
-  cmp r0, r1
-  moveq r0, #1
-  moveq r0, #0
-
-  cmp r0, #1
-  moveq r0, #'.'
-  movne r0, #'F'
-  bl putchar
-  
-  ldr r0, =1
-  push {r0, ip}
-  ldr r0, =1
-  pop {r1, ip}
-  sub r0, r0, r1
-  push {r0, ip}
-  ldr r0, =0
   pop {r1, ip}
   cmp r0, r1
   moveq r0, #1
