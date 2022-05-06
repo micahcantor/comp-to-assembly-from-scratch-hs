41c527124ba735c7c2454d10a33047f4ae3083ba
Micah Cantor <micahcantor01@gmail.com>
2022-05-05 23:08:47 -0500

    add assignment test

---
+++ data/arm/assignment.s
@@ -0,0 +1,40 @@
+
+.global main
+main:
+  push {fp, lr}
+  mov fp, sp
+  push {r0, r1, r2, r3}
+  ldr r0, =5
+  push {r0, ip}
+  ldr r0, =1
+  push {r0, ip}
+  ldr r0, [fp, #-24]
+  pop {r1, ip}
+  add r0, r0, r1
+  push {r0, ip}
+  ldr r0, =6
+  pop {r1, ip}
+  cmp r0, r1
+  moveq r0, #1
+  moveq r0, #0
+  cmp r0, #1
+  moveq r0, #'.'
+  movne r0, #'F'
+  bl putchar
+  ldr r0, =7
+  str r0, [fp, #-24]
+  ldr r0, =1
+  push {r0, ip}
+  ldr r0, [fp, #-24]
+  pop {r1, ip}
+  add r0, r0, r1
+  push {r0, ip}
+  ldr r0, =8
+  pop {r1, ip}
+  cmp r0, r1
+  moveq r0, #1
+  moveq r0, #0
+  bl asssert
+  mov sp, fp
+  mov r0, #0
+  pop {fp, pc}
---
+++ data/js/assignment.js
@@ -0,0 +1,6 @@
+function main() {
+  var n = 5;
+  assert(1 + n == 6);
+  n = 7;
+  asssert(1 + n == 8);
+}
